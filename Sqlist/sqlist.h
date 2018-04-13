a5e7309e79a990505365e6630352151f408041b7
Eric Choo <eric.choo1997@yahoo.com>
2018-04-13 21:57:35 +0800

    update sqlist.h add methods

--- Sqlist/sqlist.h
+++ Sqlist/sqlist.h
@@ -30,6 +30,39 @@ void printSqlist(Sqlist L) {
     for (int i = 0; i < L.length; ++i) {
         printf("%d ", L.data[i]);
     }
+    printf("\n");
+}
+
+int findEle(Sqlist L, int num) {
+    for (int j = 0; j < L.length; ++j) {
+        if (num == L.data[j]) {
+            return j;
+        }
+    }
+    return -1;
+}
+
+int insertSqlist(Sqlist *L, int p, int num) {
+    if (p < 0 || p > L->length || L->length == MAXSIZE) {
+        return 0;
+    }
+    for (int i = L->length - 1; i >= p; --i) {
+        L->data[i + 1] = L->data[i];
+    }
+    L->data[p] = num;
+    ++(L->length);
+    return 1;
+}
+
+int deleteSqlist(Sqlist *L, int p) {
+    if (p < 0 || p > L->length - 1) {
+        return 0;
+    }
+    for (int i = p; i < L->length - 1; ++i) {
+        L->data[i] = L->data[i + 1];
+    }
+    --(L->length);
+    return 1;
 }
 
 #endif //DATA_STRUCTURE_SQLIST_H
--- main.c
+++ main.c
@@ -5,11 +5,15 @@
 #include "list.h"
 #include "sqlist.h"
 
-int main(){
+int main() {
     Sqlist L = createSqlist();
     appendSqlist(&L, 10);
     appendSqlist(&L, 20);
     appendSqlist(&L, 30);
+    printSqlist(L);
+    insertSqlist(&L, 1, 50);
+    printSqlist(L);
+    deleteSqlist(&L, 0);
     printSqlist(L);
     return 0;
 }
