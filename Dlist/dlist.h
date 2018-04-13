9a8a6821049e3814093b4053eb28da2081e7adf7
Eric Choo <eric.choo1997@yahoo.com>
2018-04-13 22:56:54 +0800

    updated

--- Dlist/dlist.h
+++ Dlist/dlist.h
@@ -5,10 +5,22 @@
 #ifndef DATA_STRUCTURE_DLIST_H
 #define DATA_STRUCTURE_DLIST_H
 
+#include "stdlib.h"
+#include "stdio.h"
+
 typedef struct DLNode {
     int element;
     struct DLNode *prior;
     struct DLNode *next;
 } DLNode;
+
+DLNode *createDLNode(int element) {
+    DLNode *L;
+    L = (DLNode *) malloc(sizeof(DLNode));
+    L->element = element;
+    L->prior = NULL;
+    L->next = NULL;
+    return L;
+}
 
 #endif //DATA_STRUCTURE_DLIST_H
--- main.c
+++ main.c
@@ -4,16 +4,20 @@
 
 #include "list.h"
 #include "sqlist.h"
+#include "dlist.h"
 
 int main() {
-    Sqlist L = createSqlist();
-    appendSqlist(&L, 10);
-    appendSqlist(&L, 20);
-    appendSqlist(&L, 30);
-    printSqlist(L);
-    insertSqlist(&L, 1, 50);
-    printSqlist(L);
-    deleteSqlist(&L, 0);
-    printSqlist(L);
+//    Sqlist L = createSqlist();
+//    appendSqlist(&L, 10);
+//    appendSqlist(&L, 20);
+//    appendSqlist(&L, 30);
+//    printSqlist(L);
+//    insertSqlist(&L, 1, 50);
+//    printSqlist(L);
+//    deleteSqlist(&L, 0);
+//    printSqlist(L);
+
+    DLNode *L = createDLNode(20);
+    printf("%d", L->element);
     return 0;
 }
