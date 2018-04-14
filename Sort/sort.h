6f8ce1b66ac300f7f716acdf7126bc6b4c5c6c8c
Eric Choo <eric.choo1997@yahoo.com>
2018-04-14 12:55:55 +0800

    modified sort.h and cmake

--- Sort/sort.h
+++ Sort/sort.h
@@ -5,9 +5,9 @@
 #ifndef DATA_STRUCTURE_SORT_H
 #define DATA_STRUCTURE_SORT_H
 
-void insertSort(int *array, int n) {
+void insertSort(int *array, int length) {
     int j, temp;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < length; ++i) {
         temp = array[i];
         j = i;
         while (j >= 0 && temp < array[j - 1]) {
@@ -18,15 +18,15 @@ void insertSort(int *array, int n) {
     }
 }
 
-void binaryInsertSort(int *array, int n) {
-    int i, j, mid, low, high, tmp;
-    for (i=1; i < n; i++) {
-        tmp = array[i];
+void binaryInsertSort(int *array, int length) {
+    int i, j, mid, low, high, temp;
+    for (i=1; i < length; i++) {
+        temp = array[i];
         low = 0;
         high = i-1;
         while (low <= high) {
             mid = (low + high) / 2;
-            if (tmp > array[mid]) {
+            if (temp > array[mid]) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
@@ -35,7 +35,7 @@ void binaryInsertSort(int *array, int n) {
         for (j = i-1; j >= low; j--) {
             array[j + 1] = array[j];
         }
-        array[low] = tmp;
+        array[low] = temp;
     }
 }
 
